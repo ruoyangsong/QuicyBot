d1ce1dd461252ea831d2e03ae72b3bdd3bf6933c
Zhimao Lin <zhimao@ualberta.ca>
2019-01-12 15:19:11 -0700

    homepage

--- QuicyBot/src/app/home/home.module.ts
+++ QuicyBot/src/app/home/home.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from './home.component';
 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 @NgModule({
   imports: [
     CommonModule,
-    ButtonModule
+    ButtonModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   declarations: [
     HomeComponent
