80529ec8bbfc967c91423e17626b7b42557873dc
Zhimao Lin <zhimao@ualberta.ca>
2019-01-11 18:50:43 -0700

    Setup primeng

--- QuicyBot/src/app/app.module.ts
+++ QuicyBot/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
