a9ccd0dc5582289cc9abdecc1d174273ce724d35
rsong-va <rsong@vendasta.com>
2019-01-12 16:37:46 -0700

    hook up with Tom

--- QuicyBot/src/app/app-routing.module.ts
+++ QuicyBot/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import {InputBarComponent} from './input-bar/input-bar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'input', component: InputBarComponent }
 ];
 
 @NgModule({
