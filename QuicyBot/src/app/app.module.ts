6f3329c66b9cf4e079ee3663eecc95a89fc525f2
Zhimao Lin <zhimao@ualberta.ca>
2019-01-12 13:52:20 -0700

    Animation homepage

--- QuicyBot/src/app/app.module.ts
+++ QuicyBot/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {ButtonModule} from 'primeng/button';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
